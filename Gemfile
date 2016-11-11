595b79dd91836cd2ec34f5ad0f719dce7b44059e
sou <sou.phphp@gmail.com>
2016-11-11 09:37:40 +0900

    updated

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'rake',  '~> 10.0'
 gem 'pry'
+gem 'jbuilder'
--- main.rb
+++ main.rb
@@ -1,68 +1,180 @@
 #!/usr/bin/env ruby
 
 require 'pry'
+require 'yaml'
+require 'net/http'
+require 'erb'
+require 'json'
 
 $config = {
   :prompt => "client > "
 }
+$indent = ""
 
-$env = {
-  :empty => {}
-}
-$envkey = :empty
+# Struct.new("Endpoint", :scheme, :host, :port, :path)
 
-$indent = "  "
+
+class ERB
+  def result_hash hash
+    b = binding
+    eval hash.collect {|k,v| "#{k} = hash[#{k.inspect}];" }.join, b
+    result b
+  end
+end
+
+def erb template, hash
+  ERB.new(File.read(template), nil, '-').result_hash hash
+end
+
+class Environment
+
+  def initialize dir
+    @names = []
+    @sources = {}
+    Dir.glob("./#{dir}/env/*.yaml").each {|f|
+      name = File.basename f, ".yaml"
+      @names.push name
+      @sources[name.to_sym] = f
+    }
+  end
+
+  def has? name
+    @names.include? name.to_s
+  end
+
+  def ls
+    @names.each {|n|
+      yield n
+    }
+  end
+
+  def load name
+    sym = name.to_sym
+    f = @sources[sym]
+    YAML.load_file f
+  end
+end
 
 class AnyApiClient
-  def hello
-    line "hello world"
+
+  @selected = nil
+
+  def initialize appdir
+    @env = Environment.new appdir
+    @datadir = File.join appdir, 'payload'
+    @saved_vars = {}
   end
 
   def run
     describe
+    # debug
+    switch :local
+    @dat = load :get_offer_timeline
     self.pry
   end
 
   def describe
     puts  "welcome:"
     puts
-    env
+    print_env
     puts
+  end
+
+  def list
+    files = Dir.glob File.join(@datadir, "*.json")
+    files.reject! {|f| File.basename(f) == "base.json"}
+    files.map{ |f| File.basename(f, ".json").to_sym }
   end
 
   def line text
     puts "#{$indent}#{text}"
   end
 
-  def env
-    line "Environment:"
-    delim = "    "
-    Dir.glob("./env/*.yaml").each{ |f|
-      flag = File.basename(f) == File.basename($envkey.to_s) ? " *":""
-      line "#{delim}#{f}#{flag}"
-    }
-    nil
-  end
-
-  def switch env
-    unless has_env? env
-      line
-      line "#  no such environment"
-      line "#  need to specifiy file path to $env.yaml"
-      line "#  type 'env' to show available envs"
-      line
-    else
-      $envkey = env
+  def save name, json, update=false
+    path = File.join @datadir, "#{name.to_s}.json"
+    if File.exists?(path) && ! update
+      puts "cannot overwrite existing #{path} without update flag."
+      return
     end
+
+    File.open(path, "w+") {|f| f.puts JSON.pretty_generate(json) }
+    puts "saved."
   end
 
-  def has_env? name
-    names = envs.map {|e| File.basename e }
-    names.include? File.basename(name)
+  def load name
+    dat = File.join @datadir, "#{name.to_s}.json"
+    return nil unless File.exists? dat
+
+    dat = erb dat, $v
+    JSON.parse dat
   end
 
-  def envs
-    Dir.glob("./env/*.yaml")
+  def post dat, verbose=false
+    host = $endpoint.host
+    if ! $version.nil? && ! $version.empty?
+      host = "#{$version}-dot-#{host}"
+    end
+    conn = Net::HTTP.new host, $endpoint.port
+    if $endpoint.scheme == 'https'
+      conn.use_ssl = true
+    end
+
+    req = Net::HTTP::Post.new $endpoint.request_uri
+    req["Content-Type"] = "application/json"
+    req["Authorization"] = "Bearer #{$v['access_token']}"
+    $headers.each {|k,v|
+      req[k] = v
+    }
+    puts "post: >> #{host}", dat.to_json, "<<" if verbose
+    puts req["Authorization"]
+    req.body = dat.to_json
+    puts "try to request"
+    @response = conn.request req
+    puts "code: #{@response.code}"
+    puts "message: #{@response.message}"
+    puts "body:", @response.body if verbose
+    if @response.code == 200
+      @r = JSON.parse @response.body
+    end
+    JSON.parse @response.body
+  end
+
+  def print_env
+    line "environment:"
+    @env.ls { |item|
+      flag = item == @selected ? " *":""
+      line "  #{item}#{flag}"
+    }
+  end
+
+  def switch name
+    unless @env.has? name
+      puts "no such env"
+      return
+    end
+
+    if @selected
+      @saved_vars[@selected] = {
+        :endpoint => $endpoint,
+        :headers => $headers,
+        :variables => $v,
+      }
+    end
+
+    @selected = name
+
+    saved = @saved_vars[name]
+    if saved
+      $endpoint = saved[:endpoint]
+      $v = saved[:variables]
+      $version = saved[:version]
+    else
+      loaded = @env.load name
+      $endpoint = URI.parse loaded["endpoint"]
+      $headers = loaded["headers"]
+      $v = loaded["variables"]
+      $version = ""
+    end
   end
 
   def resume
@@ -76,15 +188,19 @@ Pry.config.prompt = [
   }
 ]
 
-def main env
-  cl = AnyApiClient.new
-  cl.switch(env) if env
+def main appdir
+  cl = AnyApiClient.new appdir
   cl.run
   cl.resume
 end
 
 if __FILE__ == $0
-  main ARGV.first
+  appdir = ARGV.first
+  unless appdir && File.exists?(appdir)
+    puts "need to specify application dir"
+  else
+    main appdir
+  end
 end
 
 
